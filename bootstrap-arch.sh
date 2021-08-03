6852ad768f598c65fbe36da689b8eaac67176976
Simon Vall <wahll@protonmail.ch>
2021-08-03 19:44:40 +0200

    Changed wget to curl

--- bootstrap-arch.sh
+++ bootstrap-arch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget https://github.com/simonwahll/bootstrap/blob/main/install-arch.sh
-wget https://github.com/simonwahll/bootstrap/blob/main/chroot.sh
+curl -O -J https://github.com/simonwahll/bootstrap/blob/main/install-arch.sh
+curl -O -J https://github.com/simonwahll/bootstrap/blob/main/chroot.sh
 
 chmod +x install-arch.sh chroot.sh
 
