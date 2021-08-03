2ce2afc5f18968c39f074cce909d4b597e78639f
Simon Vall <wahll@protonmail.ch>
2021-08-03 19:21:25 +0200

    Initial commit

---
+++ chroot.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+set -e
+
+# Timezone
+ln -sf "/usr/share/zoneinfo/${TIME_ZONE}" /etc/localtime
+hwclock -systohc
+
+# Localization
+echo "LANG=${LOCALE}" > /ect/locale.conf
+locale-gen
+echo "KEYMAP=${KEYMAP}" > /etc/vconsole.conf
+
+# Hostname
+echo "$NEW_HOSTNAME" > /etc/hostname
+echo "\
+127.0.0.1   localhost \
+::1         localhost \
+127.0.1.1   ${NEW_HOSTNAME}.localdomain ${NEW_HOSTNAME} \
+"
+
+# Users and passwords
+echo "Enter a password for the root user:"
+passwd
+while true
+do
+    echo "Enter a username to create a user (leave blank for no user):" read -r USERNAME
+
+    if [ "$USERNAME" = "" ]
+    then
+        break
+    fi
+
+    echo "Should this user be a member of the wheel group? (y/N)"
+    read -r WHEEL
+
+    if [ "$WHEEL" = "y" ] || [ "$WHEEL" = "Y" ]
+    then
+        useradd -m -G wheel "$USERNAME"
+    else
+        useradd -m "$USERNAME"
+    fi
+
+    echo "Enter a password for the new user:"
+    passwd "$USERNAME"
+done
+
+# Enable network manager
+systemctl enable networkmanager
+
+# Install bootloader
+if [ "$UEFI" -eq 1 ]
+then
+    bootctl install
+    
+    echo "\
+default arch.conf\
+timeout 3\
+console-mode max\
+editor no\
+" >> "${ESP_DIRECTORY}/loader/loader.conf"
+
+    echo "\
+title Arch Linux\
+linux /vmlinuz-linux\
+initrd /initramfs-linux.img\
+options root=$(blkid "$ROOT_PARTITION" | sed s/\"//g | cut -d' ' -f2) rw\
+"
+  
+    bootctl update
+else
+    echo "Non-UEFI not supported at the moment."
+    exit 1
+fi
+
+exit
---
+++ install-arch.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+
+INSTALL_PATH="/mnt"
+UEFI=1
+ESP_DIRECTORY="/boot"
+ROOT_PARTITION="/dev/nvme0n1p3"
+TIME_ZONE="Europe/Stockholm"
+LOCALE="en_US.UTF-8"
+KEYMAP="sv-latin1"
+NEW_HOSTNAME="Pluto"
+
+BASE_PACKAGES="base base-devel linux linux-firmware"
+
+# Make sure we are connected to the internet
+if [ "$(ping -c 1 archlinux.org)" -ne 1 ]
+then
+    echo "You must connect to the internet before installing."
+    echo "See the iwctl program for WiFi."
+    exit 1
+fi
+
+# Install the base system
+pacstrap "$INSTALL_PATH" $BASE_PACKAGES
+
+# Generate /etc/fstab
+genfstab -U "$INSTALL_PATH" >> "${INSTALL_PATH}/etc/fstab"
+
+# Setup system in chroot
+arch-chroot /mnt UEFI="$UEFI" ESP_DIRECTORY="$ESP_DIRECTORY" ROOT_PARTITION="$ROOT_PARTITION" TIME_ZONE="$TIME_ZONE" LOCALE="$LOCALE" KEYMAP="$KEYMAP" NEW_HOSTNAME="$NEW_HOSTNAME" ./chroot.sh
+
+# Unmount
+umount -R "$INSTALL_PATH"
